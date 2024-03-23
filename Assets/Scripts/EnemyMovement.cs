861d4ebab9092febc014fe7b581374585e5e90eb
Ravis <ravissavoy47@gmail.com>
2024-03-23 14:58:48 -0500

    Created an attack for the enemy

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    public Object target;
+    public GameObject target;
     public float speed;
-
+    public float atkRange;
     private Rigidbody rigidbody;
 
     // Start is called before the first frame update
@@ -18,15 +18,16 @@ public class EnemyMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        //Current position of traget
-        Vector3 tragetPosition = target.GetComponent<Rigidbody>().position;
-        //Distance to the traget
-        Vector3 displacement = tragetPosition - transform.position;
-        //Normalized the distance
-        displacement = displacement.normalized;
-        
-        //Moves enemy to target
-        rigidbody.MovePosition(transform.position + displacement * speed * Time.fixedDeltaTime);
+        float distanceToTarget = Vector3.Distance(transform.position, target.transform.position);
+        if (distanceToTarget > atkRange)
+        {
+            TryMove(transform.position);
+        }
+        else
+        {
+            TryAttack();
+        }
+
     }
 
     // Update is called once per frame
@@ -34,4 +35,45 @@ public class EnemyMovement : MonoBehaviour
     {
         
     }
+
+    private void TryMove(Vector3 currentPos)
+    {
+        //Current position of traget
+        Vector3 tragetPosition = target.transform.position;
+        //Distance to the traget
+        Vector3 displacement = tragetPosition - currentPos;
+        //Normalized the distance
+        displacement = displacement.normalized;
+
+        //Moves enemy to target
+        rigidbody.MovePosition(currentPos += displacement * speed * Time.fixedDeltaTime * atkRange);
+    }
+
+    private void TryAttack()
+    {
+        //create a newobject to be the hitbox
+        GameObject HitBox = new GameObject("HitBox");
+
+        // Add components to the new GameObject
+        HitBox.AddComponent<MeshRenderer>();
+        HitBox.AddComponent<MeshFilter>();
+        HitBox.AddComponent<BoxCollider>();
+
+        // Set the position of the new GameObject
+        HitBox.transform.position = transform.position + new Vector3(-atkRange, 0f, 0f);
+
+        //Checks teh position of the hitbox and the target
+        if (HitBox.transform.position == target.transform.position)
+        {
+            // Bounce the target up
+            Rigidbody targetRigidbody = target.GetComponent<Rigidbody>();
+            if (targetRigidbody != null)
+            {
+                targetRigidbody.AddForce(Vector3.up * 10f, ForceMode.Impulse);
+            }
+        }
+
+        Destroy(HitBox);
+    }
+   
 }
